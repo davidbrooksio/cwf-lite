e266779dc6a9bb3dc7efce9f776891a2d7c525d2
David Brooks <david.brooks@claritas-solutions.co.uk>
2026-02-20 10:23:37 +0000

    added banner comments

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,17 @@
 import { defineConfig } from "vite";
 import postcssEach from "postcss-each";
+import pkg from "./package.json"; // Import your package.json
+
+// Define your banner template
+// Note: We use /*! to ensure minifiers don't strip the comment
+const banner = `/*!
+ * @name ${pkg.name}
+ * @version ${pkg.version}
+ * @description ${pkg.description}
+ * @author ${pkg.author}
+ * @license ${pkg.license}
+ */
+`;
 
 export default defineConfig({
   server: {
@@ -10,11 +22,29 @@ export default defineConfig({
       plugins: [postcssEach({})],
     },
   },
+  plugins: [
+    // Custom plugin to inject the banner into CSS
+    {
+      name: "banner-plugin",
+      renderChunk(code) {
+        return banner + code;
+      },
+      generateBundle(options, bundle) {
+        for (const [fileName, asset] of Object.entries(bundle)) {
+          if (fileName.endsWith(".css")) {
+            asset.source = banner + asset.source;
+          }
+        }
+      },
+    },
+  ],
   build: {
     manifest: true,
     cssMinify: true,
     rollupOptions: {
       output: {
+        // Rollup native banner for JS files
+        banner: banner,
         entryFileNames: "[name].js",
         chunkFileNames: "[name].js",
         assetFileNames: "[name].[ext]",
